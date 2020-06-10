3685cda8b8bc657ea69dcbe548ea0e99f217db2f
Imam Syuhada <imam.sy11@gmail.com>
2020-06-10 15:15:58 +0700

    header ui view

--- iosLayananMaskam/ViewController.swift
+++ iosLayananMaskam/ViewController.swift
@@ -9,8 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet weak var tabelBarang: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
