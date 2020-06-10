b5b32ab5e5c382ff00cb029e3dd220418f3f380e
Imam Syuhada <imam.sy11@gmail.com>
2020-06-10 15:08:45 +0700

    header ui view

---
+++ iosLayananMaskam/BarangTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BarangTableViewCell.swift
+//  iosLayananMaskam
+//
+//  Created by Imam Syuhada on 10/06/20.
+//  Copyright © 2020 Imam Syuhada. All rights reserved.
+//
+
+import UIKit
+
+class BarangTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
